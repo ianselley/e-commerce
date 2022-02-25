a3b6d722957aea088164b023204bb2c4d4ecbf6f
ianselley <ianselleymartin@gmail.com>
2022-02-25 19:49:47 +0100

    Tweaks to start to make vite work

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -1,20 +1,18 @@
-import { createApp } from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
+import { createApp } from 'vue'
+import axios from 'axios'
 
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './index.css';
-import { BASE_URL } from './config.json';
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './index.css'
+import { BASE_URL } from './config.json'
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = BASE_URL;
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = BASE_URL
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(store);
-app.use(router);
-app.use(VueAxios, axios);
+app.use(store)
+app.use(router)
 
-app.mount('#app');
+app.mount('#app')
--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,18 +1,36 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import ViteRestart from 'vite-plugin-restart'
+// import { nodeResolve } from '@rollup/plugin-node-resolve'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8080,
-  },
+  // plugins: [
+  //   nodeResolve({
+  //     moduleDirectories: [path.resolve('./node_modules')],
+  //   }),
+  //   ViteRestart({
+  //     reload: ['./src/**/*'],
+  //   }),
+  //   vue(),
+  // ],
+  publicDir: './src/public',
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-});
+  define: {
+    'process.env': {},
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+    host: '0.0.0.0',
+    origin: 'http://0.0.0.0:8080/',
+    port: 8080,
+    strictPort: true,
+  },
+})
